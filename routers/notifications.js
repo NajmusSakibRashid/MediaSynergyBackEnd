7e9cdf64e13d1b944307ef1e07ef1f041ba3bfec
NajmusSakibRashid <55847267+NajmusSakibRashid@users.noreply.github.com>
2024-02-23 16:25:11 +0600

    notification modification

---
+++ routers/notifications.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const router = express.Router();
+const eventEmitter = require('../utility/event-emitter');
+
+router.get('/notifications',(req,res)=>{
+  res.setHeader('Content-Type', 'text/event-stream');
+  res.setHeader('Cache-Control', 'no-cache');
+  res.setHeader('Connection', 'keep-alive');
+  eventEmitter.on('post',(post)=>{
+    res.write(`data: ${JSON.stringify(post)}\n\n`);
+  });
+  res.on('close',()=>{
+    eventEmitter.removeListener('post',()=>{});
+  });
+})
+
+module.exports=router;
