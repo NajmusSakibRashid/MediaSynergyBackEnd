4fb6325db787e59b5dbd4651d2dfafbe3a7b6b30
SuvroGfz <1905020@ugrad.cse.buet.ac.bd>
2024-03-01 01:41:24 +0600

    post, comment

--- routers/user-router.js
+++ routers/user-router.js
@@ -10,11 +10,11 @@ const profileRouter = require("./user-routers/profile");
 // const authMiddleware=require('../middlewares/authentication');
 // const contentRouter=require('./user-routers/content');
 // const profileRouter=require('./user-routers/profile');
-const notificationRouter=require('./user-routers/notifications');
-const buildProfileRouter=require('./user-routers/build-profile');
-const fetchProfileRouter=require('./user-routers/fetchProfile');
-const communityRouter = require('./user-routers/community');
-const User=require('../models/User');
+const notificationRouter = require("./user-routers/notifications");
+const buildProfileRouter = require("./user-routers/build-profile");
+const fetchProfileRouter = require("./user-routers/fetchProfile");
+const communityRouter = require("./user-routers/community");
+const User = require("../models/User");
 
 router.use(authMiddleware);
 
@@ -37,21 +37,32 @@ router.get("/getUserId", authenticateToken, (req, res) => {
   console.log("Fetching user ID called -> User ID: ", userId);
 });
 
+router.get("/getUser/:userId", async (req, res) => {
+  // The user ID should be available in req.user from the authentication middleware
+  console.log("fetching user name");
+
+  // fetch user from database
+  const user = await User.findById(req.params.userId);
+  res.json(user);
+  // console.log(res);
+  console.log("Fetching user called -> User: ", user);
+});
+
 router.use("/content", contentRouter);
 router.use("/profile", profileRouter);
 router.use("/notifications", notificationRouter);
 router.use("/communities", communityRouter);
-router.get('/',async (req,res)=>{
-  const user=await User.findById(req.user.id)
-  if(!user) return res.status(400).send({error:'User not found'});
-  res.send({user});
-})
+router.get("/", async (req, res) => {
+  const user = await User.findById(req.user.id);
+  if (!user) return res.status(400).send({ error: "User not found" });
+  res.send({ user });
+});
 
-router.use('/content',contentRouter);
-router.use('/profile',profileRouter);
-router.use('/notifications',notificationRouter);
-router.use('/build-profile',buildProfileRouter);
-router.use('/fetch-profile',fetchProfileRouter);
-router.use('/communities',communityRouter);
+router.use("/content", contentRouter);
+router.use("/profile", profileRouter);
+router.use("/notifications", notificationRouter);
+router.use("/build-profile", buildProfileRouter);
+router.use("/fetch-profile", fetchProfileRouter);
+router.use("/communities", communityRouter);
 
 module.exports = router;
